f8d5a3108e236f57849238b85b6d2d2d3bc7d897
marcel borkowski <37640762+Sataniachia@users.noreply.github.com>
2025-11-16 03:05:02 -0500

    Update MongoDB URI to use placeholder for password

--- server/config/database.js
+++ server/config/database.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
       process.env.MONGODB_URI ||
-      "mongodb+srv://admin:7%2B26w%219a%3BniG%26QV@cluster0.vjeqfa4.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://admin:<7%2B26w%219a%3BniG%26QV>@cluster0.vjeqfa4.mongodb.net/?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
